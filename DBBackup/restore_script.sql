c41d6c86e82a4fcff54f576ec40dbf242eb4c7fa
prvacy <32227071+prvacy@users.noreply.github.com>
2019-05-30 17:38:31 +0300

    Added restore script

---
+++ DBBackup/restore_script.sql
@@ -0,0 +1,4 @@
+﻿RESTORE DATABASE productstoredb
+  FROM Disk = 'C:\Users\lenovo\source\repos\OnlineStoreMVC\DBBackup\productstoredb_v0.5.bak' 
+  WITH MOVE 'productstoredb' TO 'C:\Users\lenovo\source\repos\OnlineStoreMVC\DBBackup\db.mdf',
+  MOVE 'productstoredb_log' TO 'C:\Users\lenovo\source\repos\OnlineStoreMVC\DBBackup\db_log.ldf';
